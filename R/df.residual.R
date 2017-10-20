c2aa50d0a9b5323b8989490a3c5d2b033248ca1a
bozenne <broz@sund.ku.dk>
2017-10-20 09:20:52 +0200

    save df

--- R/df.residual.R
+++ R/df.residual.R
@@ -3,9 +3,9 @@
 ## author: Brice Ozenne
 ## created: jun 23 2017 (12:27) 
 ## Version: 
-## last-updated: okt  5 2017 (11:32) 
+## last-updated: okt 19 2017 (18:36) 
 ##           By: Brice Ozenne
-##     Update #: 25
+##     Update #: 85
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -29,30 +29,135 @@
 #' m <- lvm(Y~X1+X2+X3+X4)
 #' d <- lava::sim(m, n)
 #' e <- estimate(m, data = d)
-#' df.residual(e, conservative = FALSE)
-#' df.residual(e, conservative = TRUE)
+#' df.residual(e)
 #'
 #' 
 #' @rdname df.residual
 #' @method df.residual lvmfit
 #' @export
-df.residual.lvmfit <- function(object, conservative,...) {
+df.residual.lvmfit <- function(object, power = 1/2, adjust.residuals=TRUE,
+                               dmu.dtheta2=NULL, leverage.adj=NULL, Omega=NULL, iOmega=NULL,
+                               data=NULL,p=NULL,
+                               ...) {
 
-    n <- object$data$n
-    object.coef <- coef(object, level = 9)
-    index.keep <- which(rownames(object.coef) %in% names(coef(object)))
-    p.mean <- sum(attr(object.coef,"type")[index.keep]=="regression")
-    p.intercept <- sum(attr(object.coef,"type")[index.keep]=="intercept")
-    p.variance <- sum(attr(object.coef,"type")[index.keep]=="variance")
-
-    if(conservative){
-      p.effective <- p.mean+p.intercept+p.variance
-    }else{
-      p.effective <- p.mean+p.intercept
+### ** normalize arguments
+    if(!identical(class(object),"lvmfit")){
+        wrongClass <- paste(setdiff(class(object),"lvmfit"), collapse = " ")
+        stop("score2 is not available for ",wrongClass," objects \n")
     }
 
+    if(is.null(p)){
+        p <- pars(object)
+    }else{
+        if(is.null(names(p))){
+            stop("\'p\' must be names \n")
+        }
+        ref.tempo <- attr(coefType(object),"reference")
+        expected.names <- names(ref.tempo)[ref.tempo==FALSE] 
+        if(any(expected.names %in% names(p) == FALSE)){ ## used in  .calcDtheta
+            stop("argument \'p\' does not contain all the necessary parameters \n")
+        }
+    }
+### ** precompute quantities
+    name.endogenous <- endogenous(object)
+    n.endogenous <- length(name.endogenous)
+
+    if(is.null(data)){
+        data <- model.frame(object)
+    }
+    if(!is.matrix(data)){
+        data <- as.matrix(data)
+    }
     
-    return(n-p.effective)
+    if(is.null(Omega)){
+        Omega <- moments(object, p = p, conditional=TRUE, data = data)$C
+    }
+
+    if(is.null(iOmega)){
+        iOmega <- solve(Omega)
+    }
+
+    vcov.param <- vcov(object)
+    
+    if(adjust.residuals && (is.null(dmu.dtheta2) || is.null(leverage.adj))){
+        
+        ## derivative of the linear predictor
+        ls.calc <- .calcDtheta(object, data = data, param = p, n.param = length(p), 
+                               dmu = TRUE, dOmega = FALSE)
+
+        ## gather derivatives
+        ls.M <- lapply(ls.calc$dmu.dtheta[colnames(vcov.param)], function(iD){
+            if(is.null(iD)){
+                return(matrix(0,nrow = n,ncol=n.endogenous))
+            }else{
+                return(iD)
+            }
+        })
+        ## compute leverage values
+         if(n.endogenous==1){
+            dmu.dtheta2 <- do.call(cbind, args = ls.M)
+            leverage.adj <- 1 - diag(iOmega) * rowSums( (dmu.dtheta2 %*% vcov.param) * dmu.dtheta2)
+        }else{
+            dmu.dtheta2 <- do.call(abind::abind, args = list(ls.M, along = 3))
+
+            ls.diagH <- lapply(1:n, function(iI){ # iI <- 1
+                ## rowSums((dmu.dtheta2[iI,,] %*%  vcov.param) * dmu.dtheta2[iI,,]) * diag(iOmega)
+                dmu.dtheta2[iI,,] %*%  vcov.param %*% t(dmu.dtheta2[iI,,]) %*% diag(iOmega)
+            })
+            
+            leverage.adj <- 1 - t(do.call(cbind,ls.diagH))
+        }
+    }
+    
+
+    
+
+### ** compute degree of freedom
+    if(adjust.residuals){
+        if(n.endogenous==1){
+            df.adj <- sum(leverage.adj)
+
+            ## eigen.Omega <- eigen(Omega)
+            ## root.Omega <- eigen.Omega$vectors %*% diag(sqrt(eigen.Omega$values), nrow = n.endogenous, ncol = n.endogenous) %*% t(eigen.Omega$vectors)
+            
+            ## vec.lambda <- lapply(1:n, function(iI){ # iI <- 1
+            ##     iH <- dmu.dtheta2[iI,,drop=FALSE] %*%  vcov.param %*% t(dmu.dtheta2[iI,,drop=FALSE]) %*% iOmega
+            ##     iP <- diag(1,n.endogenous) - iH
+            ##     iD <- diag(diag(iH)^(power), nrow = n.endogenous, ncol = n.endogenous)
+            ##     iV <- root.Omega
+            ##     ia <- vcov.param %*% t(dmu.dtheta2[iI,,drop=FALSE]) %*% iOmega
+            ##     iM <- iV %*% iP %*% iD %*% t(ia) %*% ia  %*% iD %*% iP %*% iV
+            ##     return(eigen(iM)$values)
+            ## })
+            ## df.adj <- sum(unlist(vec.lambda))^2/sum(unlist(vec.lambda)^2)
+            
+        }else{
+            eigen.Omega <- eigen(Omega)
+            root.Omega <- eigen.Omega$vectors %*% diag(sqrt(eigen.Omega$values), nrow = n.endogenous, ncol = n.endogenous) %*% t(eigen.Omega$vectors)
+            ## root.Omega %*% root.Omega - Omega
+            ## round(vcov.param,10)
+            
+            vec.lambda <- lapply(1:n, function(iI){ # iI <- 1
+                iH <- dmu.dtheta2[iI,,] %*%  vcov.param %*% t(dmu.dtheta2[iI,,]) %*% iOmega
+                iP <- diag(1,n.endogenous) - iH
+                iD <- diag(diag(iH)^(power), nrow = n.endogenous, ncol = n.endogenous)
+                iV <- root.Omega
+                ia <- vcov.param %*% t(dmu.dtheta2[iI,,]) %*% iOmega
+                iM <- iV %*% iP %*% iD %*% t(ia) %*% ia  %*% iD %*% iP %*% iV
+                return(eigen(iM)$values)
+            })
+            df.adj <- sum(unlist(vec.lambda))^2/sum(unlist(vec.lambda)^2)
+            ## sapply(1:n.endogenous, function(iY){
+            ##     n - sum(sapply(ls.diagH,"[",iY))
+            ## })
+        }
+    }else{
+        df.adj <- n-1
+    }
+
+
+### ** export
+    return(df.adj)
 }
 
 #' @rdname df.residual
