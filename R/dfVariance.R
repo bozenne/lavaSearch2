78191370a5f00bea0e756473bc3da8083bf620cb
Brice Maxime Hugues Ozenne <hpl802@sund.root.ku.dk>
2017-11-16 16:52:40 +0100

    progress on df

--- R/dfVariance.R
+++ R/dfVariance.R
@@ -3,9 +3,9 @@
 ## author: Brice Ozenne
 ## created: okt 27 2017 (09:29) 
 ## Version: 
-## last-updated: nov 15 2017 (17:46) 
+## last-updated: nov 16 2017 (16:51) 
 ##           By: Brice Ozenne
-##     Update #: 137
+##     Update #: 159
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -19,6 +19,11 @@
 #' @title  Compute the degree of freedom of the variance parameters
 #' @description Compute the degree of freedom of the variance parameters
 #' @name dfVariance
+#'
+#' @param object a lvm object
+#' @param cluster the grouping variable relative to which the observations are i.i.d.
+#' @param fast should the degree of freedom be computed keeping the mean parameters fixed?
+#' 
 #' @export
 `dfVariance` <-
   function(object, ...) UseMethod("dfVariance")
@@ -26,7 +31,7 @@
 ## * dfVariance.gls
 #' @rdname dfVariance
 #' @export
-dfVariance.gls <- function(object, cluster, ...){
+dfVariance.gls <- function(object, cluster, fast = TRUE, ...){
 
     p <- .coef2(object)
     data <- getData(object)
@@ -39,21 +44,28 @@ dfVariance.gls <- function(object, cluster, ...){
     
 ### ** Define function to compute the standard errors
     calcSigma <- function(iParam){ # x <- p.obj
-        M <- attr(residuals2(object, cluster = cluster, p = iParam, data = data,
+        pp <- p
+        pp[names(iParam)] <- iParam
+        M <- attr(residuals2(object, cluster = cluster, p = pp, data = data,
                              adjust.residuals = FALSE, return.vcov.param = TRUE),
                   "vcov.param")
         return(setNames(diag(M), name.param))         
     }
 
 ### ** Compute the gradient of the function computing the standard errors
-    browser()
-    dSigma.dtheta <- numDeriv::jacobian(func = calcSigma, x = p, method = "Richardson")
+    if(fast){
+        keep.param <- setdiff(name.param,attr(.coef2(object),"mean.coef"))
+    }else{
+        keep.param <- name.param
+    }
+    jac.param <- p[keep.param]
+    dSigma.dtheta <- numDeriv::jacobian(func = calcSigma, x = jac.param, method = "Richardson")
     
 ### ** Compute degrees of freedom
 
     ## diag(vcov.param) - calcSigma(p)
     numerator <- 2*diag(vcov.param)^2
-    denom <- rowSums(dSigma.dtheta %*% vcov.param * dSigma.dtheta)
+    denom <- rowSums(dSigma.dtheta %*% vcov.param[keep.param,keep.param,drop=FALSE] * dSigma.dtheta)
     df <- numerator/denom
     alpha <- df/diag(vcov.param)
     return(df)
@@ -68,11 +80,11 @@ dfVariance.lme <- dfVariance.gls
 ## * dfVariance.lvmfit
 #' @rdname dfVariance
 #' @export
-dfVariance.lvmfit <- function(object, p = NULL, data = NULL, vcov.param = NULL,
+dfVariance.lvmfit <- function(object, fast = TRUE, vcov.param = NULL,
                               robust = FALSE, adjust.residuals = FALSE, power = 0.5, as.clubSandwich = TRUE, ...){
 
-    if(is.null(p)){p <- pars(object)}
-    if(is.null(data)){data <- model.frame(object)}
+    p <- pars(object)
+    data <- model.frame(object)
     if(is.null(vcov.param)){
         if(robust==FALSE){
             vcov.param <- vcov(object)
@@ -91,7 +103,9 @@ dfVariance.lvmfit <- function(object, p = NULL, data = NULL, vcov.param = NULL,
     if(robust == FALSE){
         if(adjust.residuals==FALSE){
             calcSigma <- function(iParam){ # x <- p.obj
-                M <- solve(information(object, p = iParam))
+                pp <- p
+                pp[names(iParam)] <- iParam
+                M <- solve(information(object, p = pp))
                 return(setNames(diag(M), name.param))         
             }
         }else{
@@ -99,7 +113,9 @@ dfVariance.lvmfit <- function(object, p = NULL, data = NULL, vcov.param = NULL,
         }
     }else{
         calcSigma <- function(iParam){ # x <- p.obj
-            M <- crossprod(iid2(object, p = iParam, data = data,
+            pp <- p
+            pp[names(iParam)] <- iParam
+            M <- crossprod(iid2(object, p = pp, data = data,
                                 adjust.residuals = adjust.residuals,
                                 power = power,
                                 as.clubSandwich = as.clubSandwich))
@@ -108,13 +124,21 @@ dfVariance.lvmfit <- function(object, p = NULL, data = NULL, vcov.param = NULL,
     }    
 
 ### ** Compute the gradient of the function computing the standard errors
-    dSigma.dtheta <- numDeriv::jacobian(func = calcSigma, x = p, method = "Richardson")
+    if(fast){
+        keep.type <- c("Lambda","B","Psi_var","Sigma_var","Psi_cov","Sigma_cov")
+        tableType <- coefType(object, as.lava=FALSE)        
+        keep.param <- tableType[!is.na(lava) & detail%in%keep.type,originalLink]
+    }else{
+        keep.param <- name.param
+    }
+    jac.param <- p[keep.param]
+    dSigma.dtheta <- numDeriv::jacobian(func = calcSigma, x = jac.param, method = "Richardson")
     
 ### ** Compute degrees of freedom
 
     ## diag(vcov.param) - calcSigma(p)
     numerator <- 2*diag(vcov.param)^2
-    denom <- rowSums(dSigma.dtheta %*% vcov.param * dSigma.dtheta)
+    denom <- rowSums(dSigma.dtheta %*% vcov.param[keep.param,keep.param,drop=FALSE] * dSigma.dtheta)
     df <- numerator/denom
     alpha <- df/diag(vcov.param)
     return(df)
--- tests/testthat/test-dfVariance.R
+++ tests/testthat/test-dfVariance.R
@@ -3,9 +3,9 @@
 ## author: Brice Ozenne
 ## created: okt 20 2017 (10:22) 
 ## Version: 
-## last-updated: nov 15 2017 (17:58) 
+## last-updated: nov 16 2017 (16:52) 
 ##           By: Brice Ozenne
-##     Update #: 55
+##     Update #: 70
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -24,35 +24,41 @@ library(lmerTest)
 context("dfVariance")
 n <- 5e1
 
-## * linear regression
-mSim <- lvm(Y1~X1+X2+X3,Y2~X2,Y3~1)
+mSim <- lvm(c(Y1~1*eta,Y2~1*eta,Y3~1*eta,eta~G+Gender,X1~1,X2~1))
+latent(mSim) <- ~eta
+categorical(mSim, labels = c("M","F")) <- ~Gender
 transform(mSim,Id~Y1) <- function(x){1:NROW(x)}
 set.seed(10)
-d <- as.data.table(sim(mSim,n))
+dW <- as.data.table(sim(mSim,n,latent = FALSE))
+setkey(dW, "Id")
+dL <- melt(dW,id.vars = c("G","Id","Gender","X1","X2"), variable.name = "time")
+setkey(dL, "Id")
 
+## * linear regression
 ## ** t test
 ## formula:
 ## df = \frac{ 2 * s_pool^2 }{ var(s_pool^2) }
 ##    = \frac{ ( s_X^2/m + s_Y^2/n )^2}{( s_X^4/(m(m-1)) + s_Y^4/(n(n-1)))}
 
 ## using the t test function
-e.ttest <- t.test(d$X1,d$X2)
+e.ttest <- t.test(dW$Y1,dW$Y2)
 e.ttest$parameter
 
 ## by hand
-sX1 <- var(d$X1)/n
-sX2 <- var(d$X2)/n
+sX1 <- var(dW$Y1)/n
+sX2 <- var(dW$Y2)/n
 df <- (sX1+sX2)^2/(sX1^2/(n-1) + sX2^2/(n-1))
 
 df-e.ttest$parameter
 
 ## ** lm
-e.lvm <- estimate(lvm(Y1~X1+X2),d)
-e.lm <- lm(Y1~X1+X2,d)
+e.lvm <- estimate(lvm(Y1~X1+X2), data = dW)
+e.lm <- lm(Y1~X1+X2, data = dW)
 
 ### *** clubSandwich
 cS.vcov <- vcovCR(e.lm, type = "CR2", cluster = d$Id)
 cS.df <- coef_test(e.lm, vcov = cS.vcov, test = "Satterthwaite", cluster = 1:NROW(d))
+cS.df
 ## cS.df$df is very suspect: should be the same for all coefficient and close to n-p
 
 ### *** dfVariance
@@ -72,16 +78,8 @@ test_that("linear regression: df",{
 })
 
 ## * mixed model
-mSim <- lvm(c(Y1~1*eta,Y2~1*eta,Y3~1*eta,eta~G+Gender))
-latent(mSim) <- ~eta
-categorical(mSim, labels = c("M","F")) <- ~Gender
-transform(mSim,Id~Y1) <- function(x){1:NROW(x)}
-set.seed(10)
-dW <- as.data.table(sim(mSim,n,latent = FALSE))
-setkey(dW, "Id")
-dL <- melt(dW,id.vars = c("G","Id","Gender"), variable.name = "time")
-setkey(dL, "Id")
 
+## ** Compound symmetry
 m <- lvm(c(Y1[mu1:sigma]~1*eta,
            Y2[mu2:sigma]~1*eta,
            Y3[mu3:sigma]~1*eta,
@@ -96,47 +94,65 @@ e.gls <- gls(value ~ time + G + Gender,
              correlation = corCompSymm(form=~ 1|Id),
              data = dL, method = "ML")
 
-## ** clubSandwich - bug
+## *** clubSandwich - bug
 expect_equal(logLik(e.lmer),logLik(e.lme))
 coef_test(e.lme, vcov = "CR0", test = "Satterthwaite", cluster = dL$Id)
 ## strange that same type of coef have very different degrees of freedom
 
-## ** lmerTest - ok
+## *** lmerTest - ok
 summary(e.lmer, ddf = "Satterthwaite")$coef
-
-## ** lava
+ 
+## *** lava - ok
 expect_equal(as.double(logLik(e.lmer)),as.double(logLik(e.lvm)))
 
 test_that("mixed mode: df",{
-    iidX <- iid2(e.lvm)
-    iidY <- iid2(e.lme)
-    range(iidX[,c(1:5,7,6)]-iidY)
+    GS <- summary(e.lmer, ddf = "Satterthwaite")$coef[,"df"]
     
-    V1 <- solve(information(e.lvm, p = pars(e.lvm)+1))
-    V2 <- attr(residuals2(e.lme, p = .coef2(e.lme)+1,
-                          adjust.residuals = FALSE, return.vcov.param = TRUE),
-               "vcov.param")
-    V1-V2
-    
-    df.adj <- dfVariance(e.lvm,
-                         robust = FALSE, adjust.residuals = FALSE)
+    df.adj.lvm <- dfVariance(e.lvm,
+                             robust = FALSE, adjust.residuals = FALSE)
+    expect_equal(as.double(GS),
+                 as.double(df.adj.lvm[1:5]))
 
-    expect_equal(as.double(summary(e.lmer, ddf = "Satterthwaite")$coef[,"df"]),
-                 as.double(df.adj[1:5]))
+    df.adj.lme <- dfVariance(e.lme,
+                             robust = FALSE, adjust.residuals = FALSE)
+    expect_equal(GS, df.adj.lme[names(GS)])
 
-    residuals2(e.gls)
-    df.lme <- dfVariance(e.lme,
-                         robust = FALSE, adjust.residuals = FALSE)
-    
-    
-    vcov.param <- attr(residuals2(e.lme, p = .coef2(e.lme)+1,
-                                  adjust.residuals = FALSE, return.vcov.param = TRUE),
-                       "vcov.param")
-    vcov.param
-    vcov(e.lme)
-    
-
+    df.adj.gls <- dfVariance(e.gls,
+                             robust = FALSE, adjust.residuals = FALSE)
+    expect_equal(GS, df.adj.gls[names(GS)], tol = 1e-4)
 })
+
+## ** Unstructured with weights
+
+m <- lvm(c(Y1~eta,Y2~eta,Y3~eta,eta~G+Gender))
+e.lvm <- estimate(m, dW)
+e.lme <- lme(value ~ time + G + Gender,
+             random = ~ 1|Id,
+             correlation = corSymm(),
+             weight = varIdent(form = ~1|time),
+             data = dL, method = "ML")
+e.gls <- gls(value ~ time + G + Gender,
+             correlation = corSymm(form=~ 1|Id),
+             weight = varIdent(form = ~1|time),
+             data = dL, method = "ML")
+
+logLik(e.lvm)
+logLik(e.lme)
+logLik(e.gls)
+
+test_that("mixed mode: df",{
+    df.adj.lvm <- dfVariance(e.lvm, fast = FALSE,
+                             robust = FALSE, adjust.residuals = FALSE)
+    df.adj.lvm
+
+    ## df.adj.lme <- dfVariance(e.lme,
+    ##                          robust = FALSE, adjust.residuals = FALSE)
+
+    df.adj.gls <- dfVariance(e.gls,
+                             robust = FALSE, adjust.residuals = FALSE)
+    df.adj.gls
+})
+
 
 #----------------------------------------------------------------------
 ### test-dfVariance.R ends here
