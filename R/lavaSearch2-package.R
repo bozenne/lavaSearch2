3a4ae358a1ff71284d8a969d83d8b5c2bc5d1bc8
Brice Maxime Hugues Ozenne <hpl802@sund.root.ku.dk>
2018-01-19 16:38:34 +0100

    remove dependency on inline

--- R/lavaSearch2-package.R
+++ R/lavaSearch2-package.R
@@ -37,7 +37,6 @@
 #' @importFrom doParallel registerDoParallel
 #' @importFrom foreach foreach
 #' @importFrom graphics par plot text
-#' @importFrom inline cfunction
 #' @importFrom MASS mvrnorm
 #' @importFrom Matrix bdiag
 #' @importFrom methods is
